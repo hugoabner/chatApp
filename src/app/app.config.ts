ef732254e903056674d4a903a73a20bde1868b64
Hugo <hugopalominogalindo974@gmail.com>
2025-02-02 02:37:14 -0500

    firebase config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,18 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp} from '@angular/fire/app';
+import { firebaseConfig } from './firebase/firebase-config';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { provideToastr } from 'ngx-toastr';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirestore(() => getFirestore()),
+    provideToastr(),
+    provideAnimations()
+  ]
 };
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,11 @@
-export const environment = {};
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBZb6dSaWKUwr1sRAJvuXDDE8y5B1AYE3g",
+    authDomain: "productos-c0ddb.firebaseapp.com",
+    projectId: "productos-c0ddb",
+    storageBucket: "productos-c0ddb.appspot.com",
+    messagingSenderId: "1043170474956",
+    appId: "1:1043170474956:web:c3d27c2ed5baca6735bb69"
+  }
+};
